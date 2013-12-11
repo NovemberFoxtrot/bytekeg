148f500ac3e6ea9cf55e1dec34c7373d56abf08b
NovemberFoxtrot <november@fox.trot>
2013-12-10 22:49:42 -0800

    okay

--- bytekeg.go
+++ bytekeg.go
@@ -15,6 +15,7 @@ type datum struct {
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	var response []interface{}
 	var session *r.Session
 
 	session, err := r.Connect(map[string]interface{}{
@@ -30,13 +31,36 @@ func main() {
 
 	r.Db("test").TableCreate("Table1").Exec(session)
 
-	for i := 0; i < 20000; i++ {
-		r.Db("test").Table("Table1").Insert(map[string]interface{}{"id": i, "total": rand.Intn(1000), "correct": 1, "incorrect": 15}).Exec(session)
+	objects := make([]interface{}, 0)
+
+	for i := 1; i < 10; i++ {
+		total := rand.Intn(1000)
+
+		object := map[string]interface{}{
+			"id":        i,
+			"total":     total,
+			"correct":   total,
+			"incorrect": total,
+		}
+
+		objects = append(objects, object)
 	}
 
-	var response []interface{}
+	// table.Insert([]interface{}{map[string]interface{}{"name": "Joe"}, map[string]interface{}{"name": "Paul"}}).RunWrite(sess)
 
-	query := r.Db("test").Table("Table1").OrderBy("total")
+	log.Println(objects)
+
+	query := r.Db("test").Table("Table1").Replace(objects)
+
+	rowz, err := query.Run(session)
+
+	log.Println(rowz)
+
+	if err != nil {
+		panic(err)
+	}
+
+	query = r.Db("test").Table("Table1").Between(1, 10).OrderBy("id")
 
 	rows, err := query.Run(session)
 
